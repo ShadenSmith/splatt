49e75ae66fcf7b095dd251f33e0bf47b2b13835e
Shaden Smith <shaden@cs.umn.edu>
2014-10-30 11:52:46 -0500

    tensors parsed and stats stored

--- src/io.c
+++ src/io.c
@@ -26,6 +26,42 @@ sptensor_t * tt_read(
   }
   tt->nnz = nnz;
 
+  /* allocate structures */
+  for(idx_t m=0; m < NMODES; ++m) {
+    tt->ind[m] = (idx_t*)malloc(nnz * sizeof(idx_t));
+  }
+  tt->vals = (val_t*)malloc(nnz * sizeof(val_t));
+
+  /* fill in tensor data */
+  rewind(fin);
+  nnz = 0;
+  char *ptr = NULL;
+  while((read = getline(&line, &len, fin)) != -1) {
+    /* skip empty and commented lines */
+    if(read > 1 && line[0] != '#') {
+      ptr = strtok(line, " \t");
+      sscanf(ptr, SS_IDX, &(tt->ind[0][nnz]));
+      for(idx_t m=1; m < NMODES; ++m) {
+        ptr = strtok(NULL, " \t");
+        sscanf(ptr, SS_IDX, &(tt->ind[m][nnz]));
+      }
+      ptr = strtok(NULL, " \t");
+      sscanf(ptr, SS_VAL, &(tt->vals[nnz]));
+      ++nnz;
+    }
+  }
+
+  /* now find the max dimension in each mode */
+  for(idx_t m=0; m < NMODES; ++m) {
+    idx_t const * const ind = tt->ind[m];
+    tt->dims[m] = 0;
+    for(idx_t n=0; n < nnz; ++n) {
+      if(ind[n] > tt->dims[m]) {
+        tt->dims[m] = ind[n];
+      }
+    }
+  }
+
   free(line);
   fclose(fin);
   return tt;
--- src/stats.c
+++ src/stats.c
@@ -1,11 +1,27 @@
 
 #include "base.h"
 #include "sptensor.h"
-
 #include "io.h"
+
+#include <math.h>
 
 void tt_stats(
   char * const fname)
 {
   sptensor_t * tt = tt_read(fname);
+
+  printf("FILE=%s\n", fname);
+  printf("DIMS="SS_IDX, tt->dims[0]);
+  for(idx_t m=1; m < NMODES; ++m) {
+    printf("x" SS_IDX, tt->dims[m]);
+  }
+  printf(" NNZ= " SS_IDX, tt->nnz);
+
+  double root = pow((double)tt->nnz, 1./(double)NMODES);
+  double density = 1.0;
+  for(idx_t m=0; m < NMODES; ++m) {
+    density *= root / (double)tt->dims[m];
+  }
+  printf(" DENSITY= %e" , density);
+  printf("\n");
 }
