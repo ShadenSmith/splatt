48e02009a033b40cf3d6bac065995d5d305c6b5d
Shaden Smith <shaden@cs.umn.edu>
2014-11-18 19:36:57 -0600

    added argp

--- apps/ttstats.c
+++ apps/ttstats.c
@@ -1,17 +1,54 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
 
 #include "../include/splatt.h"
 
+
+/* argp vars */
+char const *argp_program_version = "splatt v0.0";
+char const *argp_program_bug_address = "Shaden Smith <shaden@cs.umn.edu>";
+static char doc[] = "ttstats -- print statistics about a tensor";
+static char args_doc[] = "TENSOR";
+
+typedef struct
+{
+  char *fname;
+} splatt_args;
+
+static error_t parse_opt(
+  int key,
+  char * arg,
+  struct argp_state * state)
+{
+  splatt_args *args = state->input;
+  switch(key) {
+  case ARGP_KEY_ARG:
+    if(state->arg_num > 1) {
+      argp_usage(state);
+      break;
+    }
+    args->fname = arg;
+    break;
+  case ARGP_KEY_END:
+    if(state->arg_num < 2) {
+      argp_usage(state);
+      break;
+    }
+  }
+  return 0;
+}
+
+static struct argp argp = { 0, parse_opt, args_doc, doc };
+
 int main(int argc, char **argv)
 {
-  if(argc == 1) {
-    fprintf(stderr, "usage: %s <tensor>\n", argv[0]);
-    return EXIT_FAILURE;
-  }
+  splatt_args args;
 
-  tt_stats(argv[1]);
+  argp_parse(&argp, argc, argv, 0, 0, &args);
+
+  tt_stats(args.fname);
 
   return EXIT_SUCCESS;
 }
