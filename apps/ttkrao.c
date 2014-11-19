21f03e50556bf48e62269a81d24dd451ac4fb846
Shaden Smith <ShadenTSmith@gmail.com>
2014-11-19 15:45:57 -0600

    started adding argp to ttkrao

--- apps/ttkrao.c
+++ apps/ttkrao.c
@@ -1,8 +1,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <argp.h>
 
 #include "../include/splatt.h"
+
+char const *argp_program_version = "splatt v0.0";
+char const *argp_program_bug_address = "Shaden Smith <shaden@cs.umn.edu>";
+static char doc[] = "ttkrao -- benchmark Tensor Times Khatri-Rao product algorithms";
+static char args_doc[] = "TENSOR";
+
+typedef struct
+{
+  char *fname;
+} splatt_args;
+
+static struct argp_option options[] = {
+  {"alg", 'a', "ALGORITHM", 0, "use ALG"},
+  {"threads", 't', "THREADS", 0, "use THREADS threads during computation"},
+  {0}
+};
 
 int main(int argc, char **argv)
 {
