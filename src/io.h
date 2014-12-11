42689ddec4348b68fdbaacb8a741ebfe4d6ded46
Shaden Smith <ShadenTSmith@gmail.com>
2014-12-11 16:48:24 -0600

    formatted io.h a little nicer

--- src/io.h
+++ src/io.h
@@ -11,29 +11,23 @@
 
 
 /******************************************************************************
- * PUBLIC FUNCTIONS
+ * TENSOR FUNCTIONS
  *****************************************************************************/
 sptensor_t * tt_read_file(
-  char const * const fname);
-
-idx_t * idx_read(
-  char const * const ifname,
-  idx_t const nelems);
-
-void mat_write(
-  matrix_t const * const mat,
-  char const * const fname);
-void colmat_write(
-  matrix_t const * const mat,
   char const * const fname);
 
 void tt_write_file(
   sptensor_t const * const tt,
   FILE * fout);
+
 void tt_write(
   sptensor_t const * const tt,
   char const * const fname);
 
+
+/******************************************************************************
+ * GRAPH FUNCTIONS
+ *****************************************************************************/
 void hgraph_write_file(
   hgraph_t const * const hg,
   FILE * fout);
@@ -41,11 +35,37 @@ void hgraph_write(
   hgraph_t const * const hg,
   char const * const fname);
 
+
+
+/******************************************************************************
+ * DENSE MATRIX FUNCTIONS
+ *****************************************************************************/
+void mat_write(
+  matrix_t const * const mat,
+  char const * const fname);
+void colmat_write(
+  matrix_t const * const mat,
+  char const * const fname);
+
+
+
+/******************************************************************************
+ * SPARSE MATRIX FUNCTIONS
+ *****************************************************************************/
 void spmat_write(
   spmatrix_t const * const mat,
   char const * const fname);
 void spmat_write_file(
   spmatrix_t const * const mat,
   FILE * fout);
+
+
+
+/******************************************************************************
+ * UTILITY FUNCTIONS
+ *****************************************************************************/
+idx_t * idx_read(
+  char const * const ifname,
+  idx_t const nelems);
 
 #endif
