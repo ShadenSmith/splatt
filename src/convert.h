a3de0c880ac695617b85462aad18c209b0c02983
Shaden Smith <ShadenTSmith@gmail.com>
2015-01-22 11:22:27 -0600

    adding doxygen comments for convert

--- src/convert.h
+++ src/convert.h
@@ -10,11 +10,15 @@
 /******************************************************************************
  * STRUCTURES
  *****************************************************************************/
+
+/**
+* @brief Types of tensor conversion available.
+*/
 typedef enum
 {
-  CNV_IJK_GRAPH,
-  CNV_FIB_SPMAT,
-  CNV_FIB_HGRAPH,
+  CNV_IJK_GRAPH,  /** Convert to a tri-partite graph. */
+  CNV_FIB_SPMAT,  /** Convert to a CSR matrix whose rows are <mode> fibers. */
+  CNV_FIB_HGRAPH, /** Convert to a hypergraph whose nodes are <mode> fibers. */
   CNV_ERROR,
 } splatt_convert_type;
 
@@ -22,6 +26,16 @@ typedef enum
 /******************************************************************************
  * PUBLIC FUNCTIONS
  *****************************************************************************/
+
+
+/**
+* @brief Load a tensor, convert to a different form, and write it to disk.
+*
+* @param ifname The tensor filename.
+* @param ofname The output filename.
+* @param mode Which mode to operate on (if applicable).
+* @param type The type of conversion to perform.
+*/
 void tt_convert(
   char const * const ifname,
   char const * const ofname,
