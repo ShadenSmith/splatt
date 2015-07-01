2e65fffc48b7c79598827bb4168a65cfa02936f0
Shaden Smith <shaden@cs.umn.edu>
2015-07-01 16:45:08 -0500

    added makefile for octave

---
+++ mex/make_octave.m
@@ -0,0 +1,24 @@
+function make_octave
+c = computer
+switch c
+case 'x86_64-pc-linux-gnu'
+  mkoctfile --mex splatt_load.c -I../include -L../build/Linux-x86_64/lib ...
+      -lsplatt -lgomp -lm
+  mkoctfile --mex  splatt_cpd.c -I../include -L../build/Linux-x86_64/lib ...
+      -lsplatt -lgomp -lm
+
+% TODO: What does it use for OSX?
+case 'MACI64'
+    mkoctfile --mex splatt_load.c -I../include -L../build/Darwin-x86_64/lib ...
+    -lsplatt -lgomp -lm
+    mkoctfile --mex splatt_cpd.c -I../include -L../build/Darwin-x86_64/lib ...
+    -lsplatt -lgomp -lm
+
+% TODO: What does it do for 32-bit Linux?
+case 'GLNX32'
+    mkoctfile --mex splatt_load.c -I../include -L../build/Linux-x86/lib ...
+        -lsplatt -lgomp -lm
+    mkoctfile --mex splatt_cpd.c -I../include -L../build/Linux-x86/lib ...
+        -lsplatt -lgomp -lm
+    %mex -O -largeArrayDims
+end
