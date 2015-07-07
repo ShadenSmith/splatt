b5fcce6cc297095d839482fd33ab1cd524f3faff
Shaden Smith <shaden@cs.umn.edu>
2015-07-07 12:18:56 -0500

    starting filling splatt_mttkrp.c

--- mex/splatt_mttkrp.c
+++ mex/splatt_mttkrp.c
@@ -17,5 +17,14 @@ void mexFunction(
     int nrhs,
     mxArray const * prhs[])
 {
+  if(nrhs < 3) {
+    mexErrMsgTxt("Missing arguments. See 'help splatt_mttkrp' for usage.\n");
+    return;
+  }
 
+
+  mxArray * out = mxCreateDoubleMatrix(1, 1, mxREaL);
+  if(nlhs > 0) {
+    plhs[0] = out;
+  }
 }
