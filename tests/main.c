b7021420fadc02b3f5b8f90473a151775abb8cd4
Shaden Smith <shaden@cs.umn.edu>
2015-08-17 07:14:21 -0500

    removed 'found ...' messages from testing

--- tests/main.c
+++ tests/main.c
@@ -7,10 +7,6 @@
 
 int main(int argc, char const ** argv)
 {
-  printf("found %s\n", DATASET(small.tns));
-  printf("found %s\n", DATASET(small4.tns));
-  printf("found %s\n", DATASET(med.tns));
-  printf("found %s\n", DATASET(small.tns));
   return ctest_main(argc, argv);
 }
 
