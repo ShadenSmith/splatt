b4a063c86bc97ae725e915c243f236ee366825f8
Shaden Smith <shaden@cs.umn.edu>
2015-08-17 23:33:03 -0500

    renamed misleading tile_csf cases to tile_dense

--- tests/tile_dense_test.c
+++ tests/tile_dense_test.c
@@ -6,14 +6,14 @@
 #include "splatt_test.h"
 
 
-CTEST_DATA(tile_csf)
+CTEST_DATA(tile_dense)
 {
   sptensor_t * tt;
   idx_t ntiles;
   idx_t tile_dims[MAX_NMODES];
 };
 
-CTEST_SETUP(tile_csf)
+CTEST_SETUP(tile_dense)
 {
   data->tt = tt_read(DATASET(med4.tns));
   data->ntiles = 1;
@@ -23,7 +23,7 @@ CTEST_SETUP(tile_csf)
   }
 }
 
-CTEST_TEARDOWN(tile_csf)
+CTEST_TEARDOWN(tile_dense)
 {
   tt_free(data->tt);
 }
@@ -33,7 +33,7 @@ CTEST_TEARDOWN(tile_csf)
  * Use a basic checksum to ensure  no nnz went missing after tiling. A pass
  * does not rule out a false negative!
  */
-CTEST2(tile_csf, no_missing_nnz)
+CTEST2(tile_dense, no_missing_nnz)
 {
   idx_t cksums[MAX_NMODES];
   for(idx_t m=0; m < data->tt->nmodes; ++m) {
@@ -74,7 +74,7 @@ CTEST2(tile_csf, no_missing_nnz)
  * the tile ptr to traverse the tensor and check. A pass does not rule out a
  * false negative!
  */
-CTEST2(tile_csf, no_missing_nnz_traverse)
+CTEST2(tile_dense, no_missing_nnz_traverse)
 {
   idx_t cksums[MAX_NMODES];
   for(idx_t m=0; m < data->tt->nmodes; ++m) {
@@ -120,7 +120,7 @@ CTEST2(tile_csf, no_missing_nnz_traverse)
  * Perform a dense tiling and ensure every nnz has the index range that is
  * expected.
  */
-CTEST2(tile_csf, check_tile_bounds)
+CTEST2(tile_dense, check_tile_bounds)
 {
   idx_t * ptr = tt_densetile(data->tt, data->tile_dims);
 
@@ -155,3 +155,48 @@ CTEST2(tile_csf, check_tile_bounds)
   }
   free(ptr);
 }
+
+
+/*
+ * Perform a dense tiling and ensure every nnz has the index range that is
+ * expected.
+ */
+CTEST2(tile_dense, check_tile_bounds_weirddims)
+{
+  for(idx_t m=0; m < data->tt->nmodes; ++m) {
+    data->tile_dims[m] = m+1;
+  }
+  idx_t * ptr = tt_densetile(data->tt, data->tile_dims);
+
+  idx_t coords[MAX_NMODES];
+
+  sptensor_t const * const tt = data->tt;
+  for(idx_t i=0; i < data->tile_dims[0]; ++i) {
+    idx_t id;
+    id = get_next_tileid(TILE_BEGIN, data->tile_dims, data->tt->nmodes, 0, i);
+    while(id != TILE_END) {
+      fill_tile_coords(data->tile_dims, tt->nmodes, id, coords);
+      idx_t const startnnz = ptr[id];
+      idx_t const endnnz = ptr[id+1];
+      for(idx_t x=startnnz; x < endnnz; ++x) {
+        for(idx_t m=0; m < tt->nmodes; ++m) {
+          idx_t const tsize = tt->dims[m] / data->tile_dims[m];
+          idx_t const minidx = coords[m] * tsize;
+          idx_t const maxidx = (coords[m] + 1) * tsize;
+
+          /* assert we are in the correct range */
+          ASSERT_EQUAL(1, tt->ind[m][x] >= minidx);
+          /* last coordinate may have overflow */
+          if(coords[m]+1 < data->tile_dims[m]) {
+            ASSERT_EQUAL(1, tt->ind[m][x] < maxidx);
+          }
+        }
+      }
+
+      /* next tile */
+      id = get_next_tileid(id, data->tile_dims, tt->nmodes, 0, i);
+    }
+  }
+  free(ptr);
+}
+
