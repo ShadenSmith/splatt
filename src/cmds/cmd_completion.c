cba76451745cff699e3b8234a98b50509a66452b
Jongsoo Park <jongsoo.park@intel.com>
2016-03-20 20:41:32 -0700

    a working version of distributed sgd; still need to figure out why it doesn't converge with a few ranks (converges with 1 rank or with more than 32 ranks)

--- src/cmds/cmd_completion.c
+++ src/cmds/cmd_completion.c
@@ -34,6 +34,7 @@ static char tc_doc[] =
 #define TC_TOL 251
 #define TC_NORAND_PER_ITERATION 250
 #define TC_HOGWILD 249
+#define TC_FOLD 248
 static struct argp_option tc_options[] = {
   {"iters", 'i', "NITERS", 0, "maximum iterations/epochs (default: 500)"},
   {"rank", 'r', "RANK", 0, "rank of decomposition to find (default: 16)"},
@@ -48,6 +49,7 @@ static struct argp_option tc_options[] = {
   {"tol", TC_TOL, "TOLERANCE", 0, "converge if RMSE-vl has not improved by TOLERANCE in 20 epochs (default: 1e-4)"},
   {"norand", TC_NORAND_PER_ITERATION, 0, 0, "do not randomly permute every iteration for SGD"},
   {"hogwild", TC_HOGWILD, 0, 0, "hogwild for SGD (default no)"},
+  {"folds", TC_FOLD, "FOLDS", 0, "folds per epoch (default 1)"},
   {0}
 };
 
@@ -121,6 +123,7 @@ typedef struct
 
   bool rand_per_iteration;
   bool hogwild;
+  idx_t folds;
 } tc_cmd_args;
 
 
@@ -150,6 +153,7 @@ static void default_tc_opts(
   args->set_tolerance = false;
   args->rand_per_iteration = true;
   args->hogwild = false;
+  args->folds = 1;
 }
 
 
@@ -216,6 +220,9 @@ static error_t parse_tc_opt(
   case TC_HOGWILD:
     args->hogwild = true;
     break;
+  case TC_FOLD:
+    args->folds = atoi(arg);
+    break;
 
   case ARGP_KEY_ARG:
     if(args->nfiles == 3) {
@@ -238,6 +245,65 @@ static error_t parse_tc_opt(
 /* tie it all together */
 static struct argp tc_argp = {tc_options, parse_tc_opt, tc_args_doc, tc_doc};
 
+void tt_get_layer_dims(
+    FILE * fin,
+    idx_t * const outnmodes,
+    idx_t * const outnnz,
+    idx_t * outdims,
+    rank_info * rinfo)
+{
+  char * ptr = NULL;
+  idx_t nnz = 0;
+  char * line = NULL;
+  ssize_t read;
+  size_t len = 0;
+
+  /* first count modes in tensor */
+  idx_t nmodes = 0;
+  while((read = getline(&line, &len, fin)) != -1) {
+    if(read > 1 && line[0] != '#') {
+      /* get nmodes from first nnz line */
+      ptr = strtok(line, " \t");
+      while(ptr != NULL) {
+        ++nmodes;
+        ptr = strtok(NULL, " \t");
+      }
+      break;
+    }
+  }
+  --nmodes;
+
+  for(idx_t m=0; m < nmodes; ++m) {
+    outdims[m] = 0;
+  }
+
+  /* fill in tensor dimensions */
+  rewind(fin);
+  while((read = getline(&line, &len, fin)) != -1) {
+    /* skip empty and commented lines */
+    if(read > 1 && line[0] != '#') {
+      ptr = line;
+      bool skip = false;
+      for(idx_t m=0; m < nmodes; ++m) {
+        idx_t ind = strtoull(ptr, &ptr, 10) - 1;
+        if(m == 0 && (ind < rinfo->layer_starts[0] || ind >= rinfo->layer_ends[0])) {
+          skip = true;
+        }
+        outdims[m] = (ind + 1 > outdims[m]) ? ind + 1 : outdims[m];
+      }
+      /* skip over tensor val */
+      strtod(ptr, &ptr);
+      if (!skip) ++nnz;
+    }
+  }
+  *outnnz = nnz;
+  *outnmodes = nmodes;
+
+  outdims[0] = rinfo->layer_ends[0] - rinfo->layer_starts[0];
+
+  rewind(fin);
+  free(line);
+}
 
 /******************************************************************************
  * SPLATT-COMPLETE
@@ -249,19 +315,123 @@ int splatt_tc_cmd(
   tc_cmd_args args;
   default_tc_opts(&args);
   argp_parse(&tc_argp, argc, argv, ARGP_IN_ORDER, 0, &args);
+#ifdef SPLATT_USE_MPI
+  rank_info rinfo;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rinfo.rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &rinfo.npes);
+
+  if(rinfo.rank == 0) {
+    print_header();
+  }
+#else
   print_header();
+#endif
 
   srand(args.seed);
 
+#ifdef SPLATT_USE_MPI
+  rinfo.decomp = SPLATT_DECOMP_MEDIUM;
+  rinfo.dims_3d[0] = rinfo.npes;
+  for(idx_t d=1; d < MAX_NMODES; ++d) {
+    rinfo.dims_3d[d] = 1;
+  }
+  for(idx_t m=0; m < MAX_NMODES; ++m) {
+    rinfo.layer_starts[m] = 0;
+  }
+
+  sptensor_t * train = mpi_tt_read(args.ifnames[0], NULL, &rinfo);
+
+  sptensor_t *validate = NULL;
+  idx_t global_validate_nnz = 0;
+  {
+    FILE *fin;
+    if ((fin = fopen(args.ifnames[1], "r")) == NULL) {
+      fprintf(stderr, "SPLATT ERROR: failed to open '%s'\n", args.ifnames[1]);
+      return SPLATT_ERROR_BADINPUT;
+    }
+
+    timer_start(&timers[TIMER_IO]);
+
+    /* first count nnz in tensor */
+    idx_t nnz = 0;
+    idx_t nmodes = 0;
+
+    idx_t dims[MAX_NMODES];
+    tt_get_layer_dims(fin, &nmodes, &nnz, dims, &rinfo);
+
+    if(nmodes > MAX_NMODES) {
+      fprintf(stderr, "SPLATT ERROR: maximum %"SPLATT_PF_IDX" modes supported. "
+                      "Found %"SPLATT_PF_IDX". Please recompile with "
+                      "MAX_NMODES=%"SPLATT_PF_IDX".\n",
+              MAX_NMODES, nmodes, nmodes);
+      return SPLATT_ERROR_BADINPUT;
+    }
+
+    /* allocate structures */
+    validate = tt_alloc(nnz, nmodes);
+    memcpy(validate->dims, dims, nmodes * sizeof(*dims));
+
+    char * line = NULL;
+    int64_t read;
+    size_t len = 0;
+
+    /* fill in tensor data */
+    rewind(fin);
+    nnz = 0;
+    while((read = getline(&line, &len, fin)) != -1) {
+      /* skip empty and commented lines */
+      if(read > 1 && line[0] != '#') {
+        char *ptr = line;
+
+        bool skip = false;
+        for(idx_t m=0; m < nmodes; ++m) {
+          idx_t ind = strtoull(ptr, &ptr, 10) - 1;
+          if(0 == m) {
+            if(ind < rinfo.layer_starts[0] || ind >= rinfo.layer_ends[0]) {
+              skip = true;
+            }
+            else {
+              validate->ind[m][nnz] = ind - rinfo.layer_starts[0];
+            }
+          }
+          else if (!skip) {
+            validate->ind[m][nnz] = ind;
+          }
+        }
+        val_t val = strtod(ptr, &ptr);
+        if (!skip) {
+          validate->vals[nnz++] = val;
+          assert(nnz <= validate->nnz);
+        }
+      }
+    }
+
+    free(line);
+
+    global_validate_nnz = validate->nnz;
+    MPI_Allreduce(MPI_IN_PLACE, &global_validate_nnz, 1, SPLATT_MPI_IDX, MPI_SUM, MPI_COMM_WORLD);
+
+    timer_stop(&timers[TIMER_IO]);
+    fclose(fin);
+  }
+#else
   sptensor_t * train = tt_read(args.ifnames[0]);
   sptensor_t * validate = tt_read(args.ifnames[1]);
+#endif
+
   if(train == NULL || validate == NULL) {
     return SPLATT_ERROR_BADINPUT;
   }
   idx_t const nmodes = train->nmodes;
 
   /* print basic tensor stats */
+#ifdef SPLATT_USE_MPI
+  if(rinfo.rank == 0) {
+    mpi_global_stats(train, &rinfo, args.ifnames[0]);
+  }
+#else
   stats_tt(train, args.ifnames[0], STATS_BASIC, 0, NULL);
+#endif
 
   /* allocate model + workspace */
   tc_model * model = tc_model_alloc(train, args.nfactors, args.which_alg);
@@ -288,7 +458,11 @@ int splatt_tc_cmd(
   }
   ws->rand_per_iteration = args.rand_per_iteration;
   ws->hogwild = args.hogwild;
+  ws->folds = args.folds;
 
+#ifdef SPLATT_USE_MPI
+  if(rinfo.rank==0) {
+#endif
   printf("Factoring ------------------------------------------------------\n");
   printf("NFACTORS=%"SPLATT_PF_IDX" MAXITS=%"SPLATT_PF_IDX" ",
       model->rank, ws->max_its);
@@ -309,6 +483,9 @@ int splatt_tc_cmd(
   if(args.ifnames[2] != NULL) {
     printf("TEST=%s\n", args.ifnames[2]);
   }
+#ifdef SPLATT_USE_MPI
+  }
+#endif
 
   switch(args.which_alg) {
   case SPLATT_TC_GD:
@@ -324,8 +501,17 @@ int splatt_tc_cmd(
     splatt_tc_lbfgs(train, validate, model, ws);
     break;
   case SPLATT_TC_SGD:
-    printf("ALG=SGD\n\n");
+#ifdef SPLATT_USE_MPI
+    if(rinfo.rank==0) {
+      printf("ALG=SGD rand_per_iteration=%d hogwild=%d folds=%ld\n\n", ws->rand_per_iteration, ws->hogwild, ws->folds);
+    }
+    ws->rinfo = &rinfo;
+    ws->global_validate_nnz = global_validate_nnz;
     splatt_tc_sgd(train, validate, model, ws);
+#else
+    printf("ALG=SGD rand_per_iteration=%d hogwild=%d folds=%ld\n\n", ws->rand_per_iteration, ws->hogwild, ws->folds);
+    splatt_tc_sgd(train, validate, model, ws);
+#endif
     break;
   case SPLATT_TC_CCD:
     printf("ALG=CCD\n\n");
@@ -341,9 +527,18 @@ int splatt_tc_cmd(
     return SPLATT_ERROR_BADINPUT;
   }
 
+#ifdef SPLATT_USE_MPI
+  double mae = tc_mae(validate, ws->best_model, ws);
+  if(rinfo.rank == 0) {
+    printf("\nvalidation nnz: %"SPLATT_PF_IDX"\n", ws->global_validate_nnz);
+    printf("BEST VALIDATION RMSE: %0.5f MAE: %0.5f (epoch %"SPLATT_PF_IDX")\n\n",
+        ws->best_rmse, mae, ws->best_epoch);
+  }
+#else
   printf("\nvalidation nnz: %"SPLATT_PF_IDX"\n", validate->nnz);
   printf("BEST VALIDATION RMSE: %0.5f MAE: %0.5f (epoch %"SPLATT_PF_IDX")\n\n",
       ws->best_rmse, tc_mae(validate, ws->best_model, ws), ws->best_epoch);
+#endif
 
   tt_free(validate);
   tt_free(train);
