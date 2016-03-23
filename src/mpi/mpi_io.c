42b551b8fa0ccee444cb9a3651c551b8ac7d11d6
Shaden Smith <shaden@cs.umn.edu>
2016-03-22 19:07:56 -0500

    exposed mpi_rearrange_by_part() publicly

--- src/mpi/mpi_io.c
+++ src/mpi/mpi_io.c
@@ -184,98 +184,6 @@ static int p_determine_owner(
 }
 
 
-/**
-* @brief Rearrange nonzeros in ttbuf based on a nonzero partitioning.
-*
-* @param ttbuf The nonzeros to rearrange.
-* @param parts The partitioning.
-* @param rinfo MPI rank information.
-*
-* @return A new rearranged tensor.
-*/
-static sptensor_t * p_rearrange_by_part(
-  sptensor_t * const ttbuf,
-  int const * const parts,
-  rank_info * const rinfo)
-{
-  /* count how many to send to each process */
-  int * nsend = calloc(rinfo->npes, sizeof(*nsend));
-  int * nrecv = calloc(rinfo->npes, sizeof(*nrecv));
-  for(idx_t n=0; n < ttbuf->nnz; ++n) {
-    nsend[parts[n]] += 1;
-  }
-  MPI_Alltoall(nsend, 1, MPI_INT, nrecv, 1, MPI_INT, rinfo->comm_3d);
-
-  idx_t send_total = 0;
-  idx_t recv_total = 0;
-  for(int p=0; p < rinfo->npes; ++p) {
-    send_total += nsend[p];
-    recv_total += nrecv[p];
-  }
-  assert(send_total = ttbuf->nnz);
-
-  /* how many nonzeros I'll own */
-  idx_t nowned = recv_total;
-
-  int * send_disp = (int *) splatt_malloc((rinfo->npes+1) * sizeof(int));
-  int * recv_disp = (int *) splatt_malloc((rinfo->npes+1) * sizeof(int));
-
-  /* recv_disp is const so we'll just fill it out once */
-  recv_disp[0] = 0;
-  for(int p=1; p <= rinfo->npes; ++p) {
-    recv_disp[p] = recv_disp[p-1] + nrecv[p-1];
-  }
-
-  /* allocate my tensor and send buffer */
-  sptensor_t * tt = tt_alloc(nowned, rinfo->nmodes);
-  idx_t * isend_buf = (idx_t *) splatt_malloc(ttbuf->nnz * sizeof(idx_t));
-
-  /* rearrange into sendbuf and send one mode at a time */
-  for(idx_t m=0; m < ttbuf->nmodes; ++m) {
-    /* prefix sum to make disps */
-    send_disp[0] = send_disp[1] = 0;
-    for(int p=2; p <= rinfo->npes; ++p) {
-      send_disp[p] = send_disp[p-1] + nsend[p-2];
-    }
-
-    idx_t const * const ind = ttbuf->ind[m];
-    for(idx_t n=0; n < ttbuf->nnz; ++n) {
-      idx_t const index = send_disp[parts[n]+1]++;
-      isend_buf[index] = ind[n];
-    }
-
-    /* exchange indices */
-    MPI_Alltoallv(isend_buf, nsend, send_disp, SPLATT_MPI_IDX,
-                  tt->ind[m], nrecv, recv_disp, SPLATT_MPI_IDX,
-                  rinfo->comm_3d);
-  }
-  free(isend_buf);
-
-  /* lastly, rearrange vals */
-  val_t * vsend_buf = (val_t *) splatt_malloc(ttbuf->nnz * sizeof(val_t));
-  send_disp[0] = send_disp[1] = 0;
-  for(int p=2; p <= rinfo->npes; ++p) {
-    send_disp[p] = send_disp[p-1] + nsend[p-2];
-  }
-
-  val_t const * const vals = ttbuf->vals;
-  for(idx_t n=0; n < ttbuf->nnz; ++n) {
-    idx_t const index = send_disp[parts[n]+1]++;
-    vsend_buf[index] = vals[n];
-  }
-  /* exchange vals */
-  MPI_Alltoallv(vsend_buf, nsend, send_disp, SPLATT_MPI_VAL,
-                tt->vals,  nrecv, recv_disp, SPLATT_MPI_VAL,
-                rinfo->comm_3d);
-  free(vsend_buf);
-
-  free(nsend);
-  free(nrecv);
-  free(send_disp);
-  free(recv_disp);
-
-  return tt;
-}
 
 
 static void p_find_my_slices_1d(
@@ -593,7 +501,7 @@ static sptensor_t * p_rearrange_medium(
     parts[n] = p_determine_owner(ttbuf, n, rinfo);
   }
 
-  sptensor_t * tt = p_rearrange_by_part(ttbuf, parts, rinfo);
+  sptensor_t * tt = mpi_rearrange_by_part(ttbuf, parts, rinfo->comm_3d);
 
   splatt_free(parts);
   return tt;
@@ -619,7 +527,7 @@ static sptensor_t * p_rearrange_fine(
   /* first distribute partitioning information */
   int * parts = p_distribute_parts(ttbuf, pfname, rinfo);
 
-  sptensor_t * tt = p_rearrange_by_part(ttbuf, parts, rinfo);
+  sptensor_t * tt = mpi_rearrange_by_part(ttbuf, parts, rinfo->comm_3d);
 
   free(parts);
   return tt;
@@ -1298,5 +1206,93 @@ matrix_t * mpi_mat_rand(
 }
 
 
+sptensor_t * mpi_rearrange_by_part(
+  sptensor_t const * const ttbuf,
+  int const * const parts,
+  MPI_Comm comm)
+{
+  int rank, npes;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &npes);
+
+  /* count how many to send to each process */
+  int * nsend = calloc(npes, sizeof(*nsend));
+  int * nrecv = calloc(npes, sizeof(*nrecv));
+  for(idx_t n=0; n < ttbuf->nnz; ++n) {
+    nsend[parts[n]] += 1;
+  }
+  MPI_Alltoall(nsend, 1, MPI_INT, nrecv, 1, MPI_INT, comm);
+
+  idx_t send_total = 0;
+  idx_t recv_total = 0;
+  for(int p=0; p < npes; ++p) {
+    send_total += nsend[p];
+    recv_total += nrecv[p];
+  }
+  assert(send_total = ttbuf->nnz);
+
+  /* how many nonzeros I'll own */
+  idx_t const nowned = recv_total;
+
+  int * send_disp = splatt_malloc((npes+1) * sizeof(*send_disp));
+  int * recv_disp = splatt_malloc((npes+1) * sizeof(*recv_disp));
+
+  /* recv_disp is const so we'll just fill it out once */
+  recv_disp[0] = 0;
+  for(int p=1; p <= npes; ++p) {
+    recv_disp[p] = recv_disp[p-1] + nrecv[p-1];
+  }
+
+  /* allocate my tensor and send buffer */
+  sptensor_t * tt = tt_alloc(nowned, ttbuf->nmodes);
+  idx_t * isend_buf = splatt_malloc(ttbuf->nnz * sizeof(*isend_buf));
+
+  /* rearrange into sendbuf and send one mode at a time */
+  for(idx_t m=0; m < ttbuf->nmodes; ++m) {
+    /* prefix sum to make disps */
+    send_disp[0] = send_disp[1] = 0;
+    for(int p=2; p <= npes; ++p) {
+      send_disp[p] = send_disp[p-1] + nsend[p-2];
+    }
+
+    idx_t const * const ind = ttbuf->ind[m];
+    for(idx_t n=0; n < ttbuf->nnz; ++n) {
+      idx_t const index = send_disp[parts[n]+1]++;
+      isend_buf[index] = ind[n];
+    }
+
+    /* exchange indices */
+    MPI_Alltoallv(isend_buf, nsend, send_disp, SPLATT_MPI_IDX,
+                  tt->ind[m], nrecv, recv_disp, SPLATT_MPI_IDX,
+                  comm);
+  }
+  splatt_free(isend_buf);
+
+  /* lastly, rearrange vals */
+  val_t * vsend_buf = splatt_malloc(ttbuf->nnz * sizeof(*vsend_buf));
+  send_disp[0] = send_disp[1] = 0;
+  for(int p=2; p <= npes; ++p) {
+    send_disp[p] = send_disp[p-1] + nsend[p-2];
+  }
+
+  val_t const * const vals = ttbuf->vals;
+  for(idx_t n=0; n < ttbuf->nnz; ++n) {
+    idx_t const index = send_disp[parts[n]+1]++;
+    vsend_buf[index] = vals[n];
+  }
+  /* exchange vals */
+  MPI_Alltoallv(vsend_buf, nsend, send_disp, SPLATT_MPI_VAL,
+                tt->vals,  nrecv, recv_disp, SPLATT_MPI_VAL,
+                comm);
+  splatt_free(vsend_buf);
+  splatt_free(send_disp);
+  splatt_free(recv_disp);
+
+  /* allocated with calloc */
+  free(nsend);
+  free(nrecv);
+
+  return tt;
+}
 
 
