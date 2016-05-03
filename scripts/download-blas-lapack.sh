264600b42b60b3ba917ec0f3d37366fb50010f43
Shaden Smith <shaden@cs.umn.edu>
2016-05-02 21:57:00 -0500

    --download-blas-lapack seems to work

---
+++ scripts/download-blas-lapack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LAPACK_VERSION=3.6.0
+LAPACK_LOC=http://www.netlib.org/lapack/lapack-${LAPACK_VERSION}.tgz
+
+if [ "$#" -eq 0 ]; then
+  echo "usage: <build directory>";
+  exit 1;
+fi
+
+wget ${LAPACK_LOC} --output-document=$1/lapack.tgz;
+
+pushd $1/
+tar xf lapack.tgz
+rm lapack.tgz;
+
+mv lapack-${LAPACK_VERSION} lapack
+pushd lapack
+cmake .
+make -j
+
+popd
+popd
+
