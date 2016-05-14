315a3060c4a6c140448671aa81b171f4a4e8686c
Shaden Smith <shaden@cs.umn.edu>
2016-05-14 12:19:30 -0500

    starts ao-admm skeleton

---
+++ src/ao-admm.c
@@ -0,0 +1,46 @@
+/**
+* @file ao-admm.c
+* @brief Constrained tensor factorization using AO-ADMM.
+* @author Shaden Smith <shaden@cs.umn.edu>
+* @version 2.0.0
+* @date 2016-05-14
+*/
+
+
+
+
+/******************************************************************************
+ * INCLUDES
+ *****************************************************************************/
+#include "base.h"
+#include "csf.h"
+#include "matrix.h"
+#include "mttkrp.h"
+#include "timer.h"
+#include "thd_info.h"
+#include "util.h"
+
+
+
+
+
+
+
+
+
+/******************************************************************************
+ * API FUNCTIONS
+ *****************************************************************************/
+
+
+splatt_error_type splatt_cpd(
+    splatt_csf const * const tensor,
+    splatt_cpd_opts const * const opts,
+    splatt_kruskal * factored)
+{
+
+
+  return SPLATT_SUCCESS;
+}
+
+
