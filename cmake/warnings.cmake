df99c4daf9ed00514c06cfabf2133baa33409c66
Shaden Smith <shaden@cs.umn.edu>
2016-06-21 13:38:04 -0500

    cmake/ all filled in with '--intel' supported.

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -1,12 +1,18 @@
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wextra")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-missing-field-initializers")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-parameter")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-variable")
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-result")
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-function")
-# Unfortunately, these warnings pop up in GCC 5.1 despite our MAX_NMODES checks
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-array-bounds")
+if(SPLATT_WARNINGS)
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wextra")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-missing-field-initializers")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-parameter")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-variable")
+  #set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-result")
+  #set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-function")
+  # Unfortunately, these warnings pop up in GCC 5.1 despite our MAX_NMODES checks
+  #set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-array-bounds")
+else()
+  # turn off warning entirely for release builds
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -w")
+endif()
+
 
