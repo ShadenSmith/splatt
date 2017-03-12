fd8c5da8cc9d91ca9e57385ce2fa50dba3c3f4ee
Shaden Smith <ShadenTSmith@gmail.com>
2017-03-12 15:29:50 -0500

    Added option to download CMake for Linux/OSX. (#14)

--- scripts/download-cmake.sh
+++ scripts/download-cmake.sh
@@ -5,20 +5,23 @@ if [ "$#" -eq 0 ]; then
   exit 1;
 fi
 
+# Download location
 ARCH="$(uname -s)-$(uname -m)"
-CMAKE_URL="https://cmake.org/files/v3.6/cmake-3.6.3-${ARCH}.tar.gz"
+CMAKE_FILE="cmake-3.6.3-${ARCH}.tar.gz"
+CMAKE_URL="https://cmake.org/files/v3.6/${CMAKE_FILE}"
 CMAKE_DIR=$1/cmake
 mkdir -p ${CMAKE_DIR}
 cd ${CMAKE_DIR}
 
-# wget fails on travis-ci
-if [[ "${CI}" == "true" && "${TRAVIS}" == "true" ]]; then
-  WGET_CMD="travis_retry wget"
-else
-  WGET_CMD="wget"
+# Download binary
+curl -O ${CMAKE_URL}
+tar -f ${CMAKE_FILE} --strip-components=1 -xz -C ${CMAKE_DIR}
+
+# Apple has a different app directory than Linux
+APP_DIR=${CMAKE_DIR}
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  APP_DIR="${CMAKE_DIR}/CMake.app/Contents"
 fi
 
-${WGET_CMD} --quiet ${CMAKE_URL} -O cmake.tgz
-tar -f cmake.tgz --strip-components=1 -xz -C ${CMAKE_DIR}
-
-echo "${CMAKE_DIR}/bin/cmake"
+# Return CMake binary path in STDOUT.
+echo "${APP_DIR}/bin/cmake"
