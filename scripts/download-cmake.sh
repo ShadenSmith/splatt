ea70609c6489ee5841531c0cf05f54d6655ddd7e
Shaden Smith <shaden@cs.umn.edu>
2017-03-12 13:52:15 -0500

    printing wget debugging to sort out travis-ci

--- scripts/download-cmake.sh
+++ scripts/download-cmake.sh
@@ -11,14 +11,7 @@ CMAKE_DIR=$1/cmake
 mkdir -p ${CMAKE_DIR}
 cd ${CMAKE_DIR}
 
-# wget fails on travis-ci
-if [[ "${CI}" == "true" && "${TRAVIS}" == "true" ]]; then
-  WGET_CMD="travis_retry wget"
-else
-  WGET_CMD="wget"
-fi
-
-${WGET_CMD} --quiet ${CMAKE_URL} -O cmake.tgz
+wget --debug ${CMAKE_URL} -O cmake.tgz
 tar -f cmake.tgz --strip-components=1 -xz -C ${CMAKE_DIR}
 
 echo "${CMAKE_DIR}/bin/cmake"
