5ee05e760b3f176c470a997f548072c383066d56
Shaden Smith <shaden@cs.umn.edu>
2017-03-12 13:46:28 -0500

    making download-cmake.sh more travis-ci friendly

--- scripts/download-cmake.sh
+++ scripts/download-cmake.sh
@@ -6,12 +6,19 @@ if [ "$#" -eq 0 ]; then
 fi
 
 ARCH="$(uname -s)-$(uname -m)"
-
 CMAKE_URL="https://cmake.org/files/v3.6/cmake-3.6.3-${ARCH}.tar.gz"
 CMAKE_DIR=$1/cmake
 mkdir -p ${CMAKE_DIR}
+cd ${CMAKE_DIR}
 
-wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C ${CMAKE_DIR}
+# wget fails on travis-ci
+if [[ "${CI}" == "true" && "${TRAVIS}" == "true" ]]; then
+  WGET_CMD="travis_retry wget"
+else
+  WGET_CMD="wget"
+fi
+
+${WGET_CMD} --quiet ${CMAKE_URL} -O cmake.tgz
+tar -f cmake.tgz --strip-components=1 -xz -C ${CMAKE_DIR}
 
 echo "${CMAKE_DIR}/bin/cmake"
-
