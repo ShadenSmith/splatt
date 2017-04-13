e8002bb3ede91eeb4643647d8c111ccd79334608
Shaden Smith <shaden@cs.umn.edu>
2017-04-13 07:16:00 -0500

    moving flags around

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -8,6 +8,8 @@ set(SPLATT_FLAGS "${SPLATT_FLAGS} -fPIC")
 
 if(${CMAKE_C_COMPILER_ID} STREQUAL "Intel")
   set(SPLATT_FLAGS "${SPLATT_FLAGS} -xHost")
+  set(SPLATT_FLAGS  "-restrict")
+  add_definitions(-D_XOPEN_SOURCE=600)
 else()
   set(SPLATT_FLAGS "${SPLATT_FLAGS} -march=native")
   set(SPLATT_FLAGS "${SPLATT_FLAGS} -ftree-vectorize")
